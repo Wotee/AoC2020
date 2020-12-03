69ec14a5751af4b2c5f5d1e1f6ec6ecac5a14a61
Valtteri Tarvus <valtteri6@msn.com>
2020-12-03 18:24:16 +0200

    Day 1

---
+++ Day1.fsx
@@ -0,0 +1,16 @@
+#time
+let input = System.IO.File.ReadAllLines "inputs/day1.txt" |> Array.map int
+
+let makePairs inp = List.allPairs inp inp
+
+let len = Array.length input - 1
+let pairs = [|for i in 0..len do for j in 1..len do yield (input.[i], input.[j])|]
+
+pairs
+|> Array.pick (fun (x, y) -> if x + y = 2020 then Some (x * y) else None)
+|> printfn "Part 1: %A"
+
+input
+|> Array.pick (fun i -> pairs |> Array.tryPick (fun (x, y) -> if i + x + y = 2020 then Some (x*y*i) else None))
+|> printfn "Part 2: %A"
+#time
