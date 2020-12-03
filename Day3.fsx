69c485c836d596fe5df2150754f4dd0fc472c6aa
Valtteri Tarvus <valtteri6@msn.com>
2020-12-03 18:46:53 +0200

    Day 3

---
+++ Day3.fsx
@@ -0,0 +1,25 @@
+let input = System.IO.File.ReadAllLines "inputs/day3.txt"
+
+let splittedInput = 
+    input |> Array.map Seq.toArray
+
+// Simulate "Donut" data structure
+let getItem index list = 
+    list |> Array.item (index % (list |> Array.length))
+
+let getAllLocationsWithMove dx dy = 
+    let rowAmount = Array.length input
+    let rec getLocations x y = [| yield splittedInput |> getItem y |> getItem x; if y+dy < rowAmount then yield! getLocations (x + dx) (y + dy) |]
+    getLocations dx dy
+
+let countChar x = Array.filter ((=) x) >> Array.length
+
+getAllLocationsWithMove 3 1
+|> countChar '#'
+|> printfn "Part 1: %A"
+
+// You could skip (3,1) and use precalculated part 1, but I wont do it now
+[|(1,1);(3,1);(5,1);(7,1);(1,2)|]
+|> Array.map (fun (dx, dy) -> getAllLocationsWithMove dx dy |> countChar '#' |> int64)
+|> Array.reduce (*)
+|> printfn "Part 2: %A"
