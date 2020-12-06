38ff7cfc5d45e34506a3998772a0f4415ba29c96
Valtteri Tarvus <valtteri6@msn.com>
2020-12-06 12:36:41 +0200

    Use sumBy

--- Day6.fsx
+++ Day6.fsx
@@ -1,14 +1,20 @@
 let input = System.IO.File.ReadAllText "inputs/day6.txt" |> fun s -> s.Split $"{System.Environment.NewLine}{System.Environment.NewLine}"
 
 #time
-let run f = Array.map f >> Array.sum
+let countAnyMatches : string -> int =
+    fun s -> s.Replace(System.Environment.NewLine, "")
+    >> Set.ofSeq
+    >> Set.count
 
-let countAnyMatches (s : string) = s.Replace(System.Environment.NewLine, "") |> Set.ofSeq |> Set.count
-let countAllMatches (s : string) = s.Split(System.Environment.NewLine) |> Array.map Set.ofSeq |> Set.intersectMany |> Set.count
+let countAllMatches : string -> int =
+    fun s -> s.Split(System.Environment.NewLine) 
+    >> Array.map Set.ofSeq
+    >> Set.intersectMany
+    >> Set.count
 
-run countAnyMatches input
+Array.sumBy countAnyMatches input
 |> printfn "Part 1: %A"
 
-run countAllMatches input
+Array.sumBy countAllMatches input
 |> printfn "Part 2: %A"
 #time
